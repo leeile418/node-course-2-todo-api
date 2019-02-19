76c87df4321f75dc25139ae7c22f81a153a5c3fb
Awe Hond <you@example.com>
2019-02-19 22:46:24 +0200

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,40 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+var IDtoDelete = new ObjectID();
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+
+  //DeleteMany
+  // db.collection('Todos').deleteMany({text:'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //DeleteOne
+  // db.collection('Todos').deleteOne({text:'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').deleteMany({name:'Ile'}).then((results) => {
+    console.log(JSON.stringify(results,undefined,2));
+  }, (err) => {
+    console.log('unable',err);
+  });
+
+  // db.collection('Users').findOneAndDelete({
+  //   _id : new ObjectID("5c6bf32858f142101c5ea093")
+  // }).then((results) => {
+  //   console.log(JSON.stringify(results,undefined,2));
+  // });
+
+
+  //db.close();
+});
